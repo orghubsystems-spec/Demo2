75fea2cc4e44f0c2a8c831e3e43acb6642659848
orghubsystems-spec <orghubsystems@gmail.com>
2025-10-23 09:01:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,10 @@
 self.addEventListener('install', e => {
   console.log('🧱 Service Worker zainstalowany');
-  // Nie buforujemy danych dynamicznych
-  e.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', e => {
-  console.log('🚀 Aktywacja Service Workera');
-  // Czyścimy stare cache przy każdej aktualizacji
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-  e.waitUntil(self.clients.claim());
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', e => {
-  // Wszystko zawsze pobierane z sieci (bez cache)
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
